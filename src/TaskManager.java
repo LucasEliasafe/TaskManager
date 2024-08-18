f58acfc5f3564e801597e023f1ed4cdd8c78932e
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-18 14:53:54 -0300

    Feat: Adicionar método para listar as tarefas adicionadas e se não tiver tarefas falar que não foi encontradas tarefas

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -8,6 +8,17 @@ public class TaskManager {
         System.out.println("Tarefa adicionada com sucesso!");
     }
 
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.print("Nenhuma tarefa cadastrada.");
+        } else {
+            System.out.println("Tarefas cadastradas:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get().get(i));
+            }
+        }
+    }
+
 
 }
 
