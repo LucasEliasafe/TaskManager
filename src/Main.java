e2d3beeab73f0c30fb3b722b6b7a25f38b61a7f2
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-18 20:37:50 -0300

    Feat: Adicionar novas opções dos métodos no menu

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,13 @@ public class Main {
             System.out.println("\n--- Menu ---");
             System.out.println("1. Inserir uma nova tarefa");
             System.out.println("2. Listar tarefas");
-            System.out.println("3. Marcar uma tarefa como concluída");
-            System.out.println("4. Remover tarefa");
-            System.out.println("5. Sair");
+            System.out.println("3. Listar tarefas por prioridade");
+            System.out.println("4. Marcar tarefa como concluída");
+            System.out.println("5. Remover tarefa");
+            System.out.println("6. Editar nome da tarefa");
+            System.out.println("7. Editar descrição da tarefa");
+            System.out.println("8. Editar prioridade da tarefa");
+            System.out.println("9. Sair");
             System.out.println("Escolha uma opção: ");
             option = scanner.nextInt();
             scanner.nextLine();
