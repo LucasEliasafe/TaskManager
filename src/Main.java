76da67387b1056903479c153506a370cae3d1fd3
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-18 16:06:29 -0300

    Feat: Configurar um switch case conforme a lista do menu

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,21 @@ public class Main {
         int option;
 
         do {
+            System.out.println("\n--- Menu ---");
+            System.out.println("1. Inserir uma nova tarefa");
+            System.out.println("2. Listar tarefas");
+            System.out.println("3. Marcar uma tarefa como concluída");
+            System.out.println("4. Remover tarefa");
+            System.out.println("5. Sair");
+            System.out.println("Escolha uma opção: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (option) {
+                case 1:
+                    System.out.println("Tarefa inserida com sucesso!");
+
+            }
         }
     }
 }
