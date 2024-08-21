1f228c671fed0e0a7fbbd868537976ab2ec4824a
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-20 21:02:03 -0300

    Feat: Criar classe taskmanagergui para fazer uma interface gráfica com botões

---
+++ src/TaskManagerGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class TaskManagerGUI extends JFrame {
+}
